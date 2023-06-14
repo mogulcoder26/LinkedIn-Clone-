1f146ef47932b4009f846d99b8a2dd92816e127a
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-06-14 20:07:36 +0530

    installed react/redux and setup

---
+++ linkedin-clone/src/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header (){
+    return(
+        <div className='header'>
+            <h1>This is a header</h1>
+
+        <div className="header__left">
+            AYOOO
+        </div>
+        
+
+
+
+        </div>
+    )
+}
