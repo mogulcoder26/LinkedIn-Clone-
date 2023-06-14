de48d73e6b7e22e3e3ede64650f97bbb4bb26f08
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-06-15 00:00:15 +0530

    Header complete
    Used React Props and MUI Icons

--- linkedin-clone/src/Header.js
+++ linkedin-clone/src/Header.js
@@ -7,29 +7,34 @@ import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 import { HeaderOptions } from "./HeaderOptions";
 import MapsUgcIcon from '@mui/icons-material/MapsUgc';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-export default function Header (){
-    return(
-        <div className='header'>
 
-            <div className="header__left">
-                <img src="https://www.svgrepo.com/show/354000/linkedin-icon.svg" alt="" />
+export default function Header() {
+    return (
+        <div className="container">
+
+            <div className='header'>
+            
+                <div className="header__left">
+                    <img src="https://www.svgrepo.com/show/354000/linkedin-icon.svg" alt="" />
+                </div>
+
+                <div className="header__search">
+                    <SearchIcon />
+                    <input type="text" name="" id="" />
+                </div>
+
+
+                <div className="header__right">
+                    <HeaderOptions Icon={HomeIcon} title='Home' />
+
+                    <HeaderOptions Icon={SupervisorAccountIcon} title='My Network' />
+                    <HeaderOptions Icon={BusinessCenterIcon} title='Jobs' />
+                    <HeaderOptions Icon={MapsUgcIcon} title='Messaging' />
+                    <HeaderOptions Icon={NotificationsNoneIcon} title='Notifications' />
+                    <HeaderOptions Avatar={'https://www.hollywoodreporter.com/wp-content/uploads/2022/04/Aaron-Paul-Westworld-S3-Arrivals-GettyImages-1210694958-H-2022.jpg?w=1296'} title='Me' />
+
+                </div>
             </div>
-
-            <div className="header__search">
-            <SearchIcon />
-                <input type="text" name="" id="" />
-            </div>
-
-
-            <div className="header__right">
-                <HeaderOptions Icon = {HomeIcon} title= 'Home' />
-                
-                <HeaderOptions Icon = {SupervisorAccountIcon} title= 'My Network' /> 
-                <HeaderOptions Icon = {BusinessCenterIcon} title= 'Jobs' /> 
-                <HeaderOptions Icon = {MapsUgcIcon} title= 'Messaging' /> 
-                <HeaderOptions Icon = {NotificationsNoneIcon} title= 'Notifications' /> 
-            </div>
-   
         </div>
     )
 }
--- linkedin-clone/src/HeaderOptions.js
+++ linkedin-clone/src/HeaderOptions.js
@@ -6,7 +6,7 @@ export function HeaderOptions(props){
 
 return (
             <div className="header__Options">
-                {<props.Icon className = "headerOptions__icon" />}
+                {(props.Icon)?<props.Icon className = "headerOptions__icon" />:<img src= {props.Avatar} className= 'headerOptions__avatar'/>}
                 <h3>{props.title}</h3>
             </div>
     )
