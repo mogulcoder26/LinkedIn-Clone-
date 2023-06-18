9f1b5a79251c2e6fdf31a5b1298d88840188e81e
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-06-18 10:39:41 +0530

    init files

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Appbar , Toolbar } from '@mui/material';
+import
+
+function Header() {
+  return (
+    <div>
+    <Appbar/>
+    <Toolbar/>
+    </div>
+  )
+}
+
+export default Header
